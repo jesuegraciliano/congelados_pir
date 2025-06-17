82adc57f4c8a1acd603a3b7211c70fb0179ae31f
JESUEGRACILIANO <113379356+jesuegraciliano@users.noreply.github.com>
2025-06-16 22:25:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function calculaCargaTermica() {
   const motores     = 5.6 * L * C * H;
   const pessoas     = 395;
   const iluminacao  = 10 * areaPiso;
-  const fatorInf    = 1.146 * volume ** -0.584;
+  const fatorInf    = 0.00014
   const infiltracao = 1000 * fatorInf * volume * 120;
 
   // Cálculo da carga térmica total
